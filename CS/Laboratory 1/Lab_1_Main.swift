fa2da5bd0f8514542c98c04daeee9ebd0509d1d5
VintusS <dragomir.mindrescu@isa.utm.md>
2024-10-08 02:48:14 +0300

    Task 1.2 init

--- CS/Laboratory 1/Lab_1_Main.swift
+++ CS/Laboratory 1/Lab_1_Main.swift
@@ -14,26 +14,46 @@ let lowerCaseAlphabet = "abcdefghijklmnopqrstuvwxyz"
 
 func Laboratory1() {
     
-    // MARK: - Task 1.1
     var flag: Bool = true
+    var flag0: Bool = true
     
-    print("Choose one operation:")
-    print("1.encryption   |   2.decryption")
+    print("Choose crypto mode:")
+    print("1.easy   |   2.hard")
     
     while(flag) {
-        let keyPress = getKeyPress()
         
-        if keyPress == "1" {
-            Encryption()
+        let mode = getKeyPress()
+        
+        if mode == "1" {
+            // MARK: - Task 1.1
+            
+            print("Choose one operation:")
+            print("1.encryption   |   2.decryption")
+            
+            while(flag0) {
+                let keyPress = getKeyPress()
+                
+                if keyPress == "1" {
+                    Encryption()
+                    flag0 = false
+                } else if keyPress == "2" {
+                    Decryption()
+                    flag0 = false
+                } else {
+                    print("\nInvalid input. Please choose '1' or '2'.")
+                }
+            }
+            
             flag = false
-        } else if keyPress == "2" {
-            Decryption()
+        } else if mode == "2" {
+            // MARK: - Task 1.2
+            //HARDMODE
+            
             flag = false
         } else {
             print("\nInvalid input. Please choose '1' or '2'.")
         }
+        
     }
-    
-    // MARK: - Task 1.2
     
 }
