8ea34e7860b70ed2594cc1179555c55bb7ff6347
VintusS <dragomir.mindrescu@isa.utm.md>
2024-12-15 21:57:03 +0200

    Frequency Analysis View added. Customized

---
+++ CS/Laboratory 2/Laboratory 2/Laboratory_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Laboratory_2App.swift
+//  Laboratory 2
+//
+//  Created by Dragomir Mindrescu on 15.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Laboratory_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
